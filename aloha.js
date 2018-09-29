fff6ffddcc65e0f61d7aebd058a39e153f085f33
depintos <depintos@gmail.com>
2018-09-28 21:19:45 -0400

    third commit

--- aloha.js
+++ aloha.js
@@ -1,11 +1,63 @@
-console.log ("aloha");
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-document.querySelector("navlink").scrollIntoView({ 
-  behavior: 'smooth' 
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
 
-<script>
-  $(document).ready(function(){
-    $('.slider').bxSlider();
-  });
-</script> 
+$(function() {
+$('#submitform').on('submit', function(event) {
+   event.preventDefault();
+   if ( $('#email').val() == '' ) {
+      alert('Please enter a valid e-mail address.');
+   } else {
+      alert('You are now subscribed to our newsletter, thanks!');
+   }
+});
+});
+
+
+
+$(function() {
+            var cartItems = 0;
+        $('.product').on('click', 'button', function(event) {
+            event.preventDefault();
+            cartItems++;
+            $('.cart-items').text(cartItems).show();
+        });
+    });
+
+
+    
+
+
+
+
+
+
+
+   
+
+    
+
+    
+
+    
